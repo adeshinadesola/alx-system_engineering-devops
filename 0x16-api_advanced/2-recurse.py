29d9fe30f7e8f921a96264aafb463bb591c10a6e
Adeshina Adesola <adesolaadeshina112@outlook.com>
2023-08-09 02:10:21 -0700

    my first commit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """returns a list containing the titles of all hot articles
+       for a given subreddit"""
+
+    base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {  # Corrected indentation here
+        'User-Agent': ('0x16-api_advanced:project:'
+                       'v1.0.0 (by /u/folajomi_a)')
+    }  # And here
+
+    # If we have an "after" parameter from previous call, append it to the URL
+    if after:
+        base_url += '?after={}'.format(after)
+
+    response = requests.get(base_url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return None
+
+    try:
+        data = response.json()
+        after = data.get("data", {}).get("after", None)
+        posts = data.get("data", {}).get("children", [])
+
+        for post in posts:
+            hot_list.append(post.get("data", {}).get("title", ""))
+
+        # If there's no more data (after is None), we've obtained all the posts
+        if not after:
+            return hot_list
+
+        return recurse(subreddit, hot_list, after)
+    except json.JSONDecodeError:
+        return None
