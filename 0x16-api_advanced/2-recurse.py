c562ea17686dcff309eaad4324926126b3a5ce90
Adeshina Adesola <adesolaadeshina112@outlook.com>
2023-08-09 02:43:14 -0700

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,47 +1,32 @@
 #!/usr/bin/python3
-"""
-Contains the 2-recurse function
-"""
-
+"""Contains recurse function"""
 import requests
-import json
 
 
-def recurse(subreddit, hot_list=None, after=None):
-    """returns a list containing the titles of all hot articles
-       for a given subreddit"""
-    if hot_list is None:
-        hot_list = []
-
-    base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         'User-Agent': ('0x16-api_advanced:project:'
                        'v1.0.0 (by /u/folajomi_a)')
     }
 
-    params = {}
-    if after:
-        params['after'] = after
-
-    response = requests.get(base_url,    # Move this line outside the conditional
-                            headers=headers,
-                            params=params,
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-
-    if response.status_code != 200:
+    if response.status_code == 404:
         return None
 
-    try:
-        data = response.json()
-        after = data.get("data", {}).get("after", None)
-        posts = data.get("data", {}).get("children", [])
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
 
-        for post in posts:
-            hot_list.append(post.get("data", {}).get("title", ""))
-
-        if not after:
-            return hot_list
-
-        return recurse(subreddit, hot_list, after)
-    except json.JSONDecodeError:
-        return None
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
