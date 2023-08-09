8423b36a03ab7ea68cda3b49457077ba86e7c734
Adeshina Adesola <adesolaadeshina112@outlook.com>
2023-08-09 00:38:35 -0700

    my first commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
 
-def number_of_subscribers(subreddit):
-    """Return the number of subscribers for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "custom"
-    }
-    
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    # Check if the response status is 200 (OK)
-    if response.status_code != 200:
-        return 0
-    return response.json().get("data", {}).get("subscribers", 0)
 
-if __name__ == "__main__":
-    print(number_of_subscribers("programming"))
-    print(number_of_subscribers("this_is_a_fake_subreddit"))
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+
+    headers = { headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()}
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200 and response.headers.get('content-type') == 'application/json; charset=UTF-8':
+            data = response.json()
+            return data.get("data", {}).get("subscribers", 0)
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
+
