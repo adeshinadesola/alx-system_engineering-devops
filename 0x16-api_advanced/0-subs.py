f701ee3484a953cb9be003531f8d3ac7a9daa40a
Adeshina Adesola <adesolaadeshina112@outlook.com>
2023-08-09 01:36:39 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,19 +7,18 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/folajomi_a)'}
-    
+    headers = {
+        'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/folajomi_a)'
+    }
+
     try:
         r = requests.get(url, headers=headers, allow_redirects=False)
-        r.raise_for_status()  # This will raise an error if the HTTP status code is 4xx or 5xx
-
+        r.raise_for_status()
         data = r.json()
         return data.get("data", {}).get("subscribers", 0)
     except requests.RequestException:
         return 0
-
